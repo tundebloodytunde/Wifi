d25392b4182ff82dbfce1c3230b4018781b268c8
tundebloodytunde <6qvsyrsb9f@privaterelay.appleid.com>
2026-06-17 12:23:05 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "WIfI Calculator",
+  description: "Simple WIfI clinical calculator",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
